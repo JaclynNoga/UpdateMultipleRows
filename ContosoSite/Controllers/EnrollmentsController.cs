5a5dd0acb3571f9ac46a2e5cc52e192a90fb91a3
Jaclyn Noga <jnoga@haines.com>
2016-10-26 11:16:43 -0400

    bulk updated of table works in enrollment view. drop down tables do not work

--- ContosoSite/Controllers/EnrollmentsController.cs
+++ ContosoSite/Controllers/EnrollmentsController.cs
@@ -63,40 +63,83 @@ namespace ContosoSite.Controllers
             return View(enrollment);
         }
 
-        // GET: Enrollments/Edit/5
-        public ActionResult Edit(int? id)
+        //GET: Enrollments/Edit
+        [HttpGet]
+        public ActionResult Edit()
         {
-            if (id == null)
+            List<Enrollment> model = new List<Enrollment>();
+            using (ContosoUniversityDataEntities dc = new ContosoUniversityDataEntities())
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                model = dc.Enrollments.ToList();
             }
-            Enrollment enrollment = db.Enrollments.Find(id);
-            if (enrollment == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
-            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName", enrollment.StudentID);
-            return View(enrollment);
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName");
+            return View(model);
         }
+
+        [HttpPost]
+        public ActionResult Edit(List<Enrollment> list)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ContosoUniversityDataEntities dc = new ContosoUniversityDataEntities())
+                {
+                    foreach(var i in list)
+                    {
+                        var c = dc.Enrollments.Where(a => a.EnrollmentID.Equals(i.EnrollmentID)).FirstOrDefault();
+                        if (c != null)
+                        {
+                            c.Grade = i.Grade;
+                            c.CourseID = i.CourseID;
+                            c.StudentID = i.StudentID;
+                        }
+                    }
+                    dc.SaveChanges();
+                }
+                ViewBag.Message = "Successfully Updated.";
+                return View(list);
+            }
+            else
+            {
+                ViewBag.Message = "Failed! Please try again.";
+                return View(list);
+            }
+        }
+
+        // GET: Enrollments/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Enrollment enrollment = db.Enrollments.Find(id);
+        //    if (enrollment == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+        //    ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName", enrollment.StudentID);
+        //    return View(enrollment);
+        //}
 
         // POST: Enrollments/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EnrollmentID,Grade,CourseID,StudentID")] Enrollment enrollment)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(enrollment).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
-            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName", enrollment.StudentID);
-            return View(enrollment);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "EnrollmentID,Grade,CourseID,StudentID")] Enrollment enrollment)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(enrollment).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+        //    ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName", enrollment.StudentID);
+        //    return View(enrollment);
+        //}
 
         // GET: Enrollments/Delete/5
         public ActionResult Delete(int? id)
